9e0ceac7d1f8348bab600bb93e90e301741bd0c4
Isaac Hinojosa Padilla <jonihca.hp@gmail.com>
2017-08-27 14:35:12 -0500

    Commit inicial 21-08-2017 trabajo en clase_trazar un cuadrado

--- Project8/Main.cpp
+++ Project8/Main.cpp
@@ -1,22 +1,66 @@
-/***********************************************
-Materia:	Gráficas Computacionales
-Fecha:		21 de Agosto del 2017
-Autor:		A01375843 Isaac Hinojosa Padilla
-************************************************/
-
 #include <iostream>
+#include <GL/glew.h>
+#include <vector>
+#include <glm/glm.hpp>
+#include <GL/freeglut.h>
 
-#include "InputFile.h"
+GLuint vao;
+
+void Initialize()
+{
+	std::vector<glm::vec2> positions;
+	positions.push_back(glm::vec2(0.5f, -0.5f));
+	positions.push_back(glm::vec2(0.5f, 0.5f));
+	positions.push_back(glm::vec2(-0.5f, -0.5f));
+	positions.push_back(glm::vec2(-0.5f, 0.5f));
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	GLuint positionsVBO;
+	glGenBuffers(1, &positionsVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, positionsVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * positions.size(), positions.data(), GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
+}
+
+void GameLoop()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glBindVertexArray(vao);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+	glBindVertexArray(0);
+
+	glutSwapBuffers();
+}
 
 int main(int argc, char* argv[])
 {
-	std::string filename = "Prueba.txt";   
-	InputFile myFile;   
-	myFile.Read(filename);   
-	std::string contents = myFile.GetContents();
-	std::cout << "Contents: " << contents << std::endl;
+	
+	glutInit(&argc, argv);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
 
-	std::cin.get();
+	glutInitWindowSize(400, 400);
+	glutCreateWindow("Hello World GL");
+
+	glutDisplayFunc(GameLoop);
+
+	glewInit();
+
+	glClearColor(1.0f, 1.0f, 0.5f, 1.0f);
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	Initialize();
+
+	glutMainLoop();
 
 	return 0;
 }
